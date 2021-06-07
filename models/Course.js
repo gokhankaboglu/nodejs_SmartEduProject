222c3b1f1d7ab5be724d11fbaff380c312cedaaa
Gökhan Kaboğlu <kaboglugokhan@gmail.com>
2021-06-07 19:45:08 +0300

    update github

---
+++ models/Course.js
@@ -0,0 +1,22 @@
+const mangoose = require('mangoose');
+const Schema = mangoose.Schema;
+
+const CourseSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Course = mangoose.model('Course', CourseSchema);
+module.exports = Course;
