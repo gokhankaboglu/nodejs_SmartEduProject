34340e96f6e11517cc1ca46946ee3b45bf023f02
Gökhan Kaboğlu <kaboglugokhan@gmail.com>
2021-06-09 01:30:58 +0300

    logged in

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
@@ -28,6 +30,8 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 // Routes
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
---
+++ controllers/authController.js
@@ -0,0 +1,40 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    await User.findOne({ email: email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            res.status(200).send('You are logged in !');
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
